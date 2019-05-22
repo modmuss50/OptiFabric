28511b8f8960fd90404b8e7ed28794bec3ad4d61
modmuss50 <modmuss50@gmail.com>
2019-05-22 19:53:06 +0100

    Fixes

--- src/main/java/me/modmuss50/optifabric/mod/OptifineInjector.java
+++ src/main/java/me/modmuss50/optifabric/mod/OptifineInjector.java
@@ -1,15 +1,19 @@
 package me.modmuss50.optifabric.mod;
 
 import com.chocohead.mm.api.ClassTinkerers;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.lib.ClassReader;
-import org.spongepowered.asm.lib.tree.ClassNode;
+import org.spongepowered.asm.lib.tree.*;
 
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class OptifineInjector {
 
@@ -27,6 +31,7 @@ public class OptifineInjector {
 		}
 	}
 
+	//I have no idea why and how this works, if you know better please let me know
 	public final Consumer<ClassNode> transformer = target -> {
 		try {
 
@@ -35,11 +40,39 @@ public class OptifineInjector {
 			target.methods = source.methods;
 			target.fields = source.fields;
 			target.interfaces = source.interfaces;
+			target.superName = source.superName;
+			
+			//Not sure why this is needed, seems to fix it tho
+			for (MethodNode methodNode : target.methods) {
+				for(AbstractInsnNode insnNode : methodNode.instructions.toArray()){
+					if(insnNode instanceof FrameNode){
+						FrameNode frameNode = (FrameNode) insnNode;
+						if(frameNode.local == null){
+							frameNode.local = Collections.emptyList();
+						}
+					}
+				}
+			}
+
+			// Lets make every class we touch public
+			if(!FabricLoader.getInstance().isDevelopmentEnvironment()){
+				target.access = modAccess(target.access);
+				target.methods.forEach(methodNode -> methodNode.access = modAccess(methodNode.access));
+				target.fields.forEach(fieldNode -> fieldNode.access = modAccess(fieldNode.access));
+			}
 
 		} catch (IOException e) {
 			throw new RuntimeException("Failed to extractClasses class");
 		}
 	};
+
+	private static int modAccess(int access) {
+		if ((access & 0x7) != Opcodes.ACC_PRIVATE) {
+			return (access & (~0x7)) | Opcodes.ACC_PUBLIC;
+		} else {
+			return access;
+		}
+	}
 
 	private ClassNode getSourceClassNode(ClassNode classNode) throws IOException {
 		String name = classNode.name.replaceAll("\\.", "/") + ".class";
