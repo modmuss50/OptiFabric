8fceb2f6d40264fc00feeef6f0f2bc9c31ee5d5c
modmuss50 <modmuss50@gmail.com>
2019-11-10 00:00:35 +0000

    Version bump + loader version check

--- src/main/java/me/modmuss50/optifabric/mod/OptifabricSetup.java
+++ src/main/java/me/modmuss50/optifabric/mod/OptifabricSetup.java
@@ -4,15 +4,16 @@ import com.chocohead.mm.api.ClassTinkerers;
 import me.modmuss50.optifabric.patcher.ClassCache;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
-import net.fabricmc.loader.api.Version;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.fabricmc.loader.util.version.SemanticVersionImpl;
 import net.fabricmc.loader.util.version.SemanticVersionPredicateParser;
+import net.fabricmc.loader.util.version.VersionParsingException;
 import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.asm.mixin.Mixins;
 
 import java.io.File;
+import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 @SuppressWarnings("unused")
 public class OptifabricSetup implements Runnable {
@@ -20,6 +21,8 @@ public class OptifabricSetup implements Runnable {
 	//This is called early on to allow us to get the transformers in beofore minecraft starts
 	@Override
 	public void run() {
+		if(!validateLoaderVersion()) return;
+
 		try {
 			OptifineSetup optifineSetup = new OptifineSetup();
 			Pair<File, ClassCache> runtime = optifineSetup.getRuntime();
@@ -48,20 +51,13 @@ public class OptifabricSetup implements Runnable {
 	//I check the version like this as I want to show issues on our error screen
 	private void validateIndigoVersion() {
 		try {
-			ModContainer indigoContainer = FabricLoader.getInstance().getModContainer("fabric-renderer-indigo").orElseThrow((Supplier<Throwable>) () -> new RuntimeException("Failed to get indigo's mod container, something has broke badly."));
-			Version indigoVersion = indigoContainer.getMetadata().getVersion();
-
-			Predicate<SemanticVersionImpl> predicate = SemanticVersionPredicateParser.create(">=0.1.8");
-			SemanticVersionImpl version = new SemanticVersionImpl(indigoVersion.getFriendlyString(), false);
-
-			if (!predicate.test(version)) {
+			if (!isVersionValid("fabric-renderer-indigo", ">=0.1.8")) {
 				if(!OptifabricError.hasError()){
 					OptifineVersion.jarType = OptifineVersion.JarType.INCOMPATIBE;
 					OptifabricError.setError("You are using an outdated version of Fabric (API), please update!\n\nDownload the jar from the link bellow and replace the existing Fabric (API) jar in your mods folder.", "https://www.curseforge.com/minecraft/mc-mods/fabric-api/files");
 					OptifabricError.setHelpButtonText("Download Fabric (API)");
 				}
 			}
-
 		} catch (Throwable e){
 			if(!OptifabricError.hasError()){
 				OptifineVersion.jarType = OptifineVersion.JarType.INCOMPATIBE;
@@ -69,6 +65,42 @@ public class OptifabricSetup implements Runnable {
 			}
 			throw new RuntimeException("Failed to setup optifine", e);
 		}
+	}
+
+	private boolean validateLoaderVersion() {
+		try {
+			if (!isVersionValid("fabricloader", ">=0.6.4")) {
+				if(!OptifabricError.hasError()){
+					OptifineVersion.jarType = OptifineVersion.JarType.INCOMPATIBE;
+					OptifabricError.setError("You are using an outdated version of Fabric Loader, please update!\n\nRe-run the installer, or update via your launcher. See the link for help!", "https://fabricmc.net/wiki/install");
+					OptifabricError.setHelpButtonText("Installation Instructions");
+					return false;
+				}
+			}
+		} catch (Throwable e){
+			if(!OptifabricError.hasError()){
+				OptifineVersion.jarType = OptifineVersion.JarType.INCOMPATIBE;
+				OptifabricError.setError("Failed to load optifine, check the log for more info \n\n " + e.getMessage());
+			}
+			throw new RuntimeException("Failed to setup optifine", e);
+		}
+		return true;
+	}
+
+	private boolean isVersionValid(String modID, String validVersion) throws VersionParsingException {
+		ModMetadata modMetadata = getModMetaData(modID);
+		if(modMetadata == null) {
+			throw new RuntimeException(String.format("Failed to get mod container for %s, something has broke badly.", modID));
+		}
+
+		Predicate<SemanticVersionImpl> predicate = SemanticVersionPredicateParser.create(validVersion);
+		SemanticVersionImpl version = new SemanticVersionImpl(modMetadata.getVersion().getFriendlyString(), false);
+		return predicate.test(version);
+	}
+
+	private ModMetadata getModMetaData(String modId) {
+		Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(modId);
+		return modContainer.map(ModContainer::getMetadata).orElse(null);
 	}
 
 }
